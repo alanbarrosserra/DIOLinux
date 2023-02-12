81275f6ddaa78109b3440ea1daa4732a6fb61e59
alanbarrosserra <alanbarrosserra9@gmail.com>
2023-02-12 17:54:59 -0300

    Update criar_users.sh

--- criar_users.sh
+++ criar_users.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
 
 
-echo "Criar users do sistema...."
+echo "Criar users no system.."
 
 useradd guest10 -c "User convidado" -s /bin/bash -m -p $(openssl passwd -crypt Pass123)
 passwd guest10 -e
 
+echo "user guest10 criado com sucesso!"
+
 useradd guest11 -c "User convidado" -s /bin/bash -m -p $(openssl passwd -crypt Pass123)
 passwd guest11 -e
+
+echo "user guest11 criado com sucesso!"
 
 useradd guest12 -c "User convidado" -s /bin/bash -m -p $(openssl passwd -crypt Pass123)
 passwd guest12 -e
 
+echo "user guest12 criado com sucesso!"
+
 useradd guest13 -c "User convidado" -s /bin/bash -m -p $(openssl passwd -crypt Pass123)
 passwd guest13 -e
+
+echo "user guest13 criado com sucesso!"
 
 echo "Acabou!!"
